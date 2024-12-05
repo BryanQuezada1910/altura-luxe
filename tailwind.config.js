3213b63404c5ce7bab78b950274a53fc0ae7f784
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-12-05 01:48:16 -0600

    Add tailwind and create the project structure

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#D4AF37',
+        dark: '#1A1A1A',
+        light: '#F5F5F5',
+      },
+    },
+  },
+  plugins: [],
+};
